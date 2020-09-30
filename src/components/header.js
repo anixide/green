e5fc86a3448a52a878f5af2f6d22a69d084d7c9b
g0thman <g.othman.anis@gmail.com>
2020-09-30 23:21:09 +0100

    Header and navbar done and responsive

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,14 @@ import HeaderImage from '../components/headerImage'
 
 const header = props => {
     return (
-        <div>
-            <header className="container mx-auto flex max-w-3/4 justify-around mt-20">
-                <div className="flex flex-col justify-items justify-between gap-4">
+        <div className=" bg-gray-200">
+            <header className="Large_Screen flex flex-col-reverse md:flex-row container mx-auto max-w-3/4 justify-around mt-20">
+                <div className=" md:block  flex flex-col justify-between mr-1">
                     <h1 className="text-5xl font-bold text-gray-700"> 
                     <span className="text-green-400">Fresh</span> vegetables delivered to your doorstep</h1>
-                    <button className=" mx-4 my-2 px-6 py-2 text-center cursor-pointer rounded-lg bg-green-500 hover:bg-green-400 text-gray-100 font-semibold">
-                        <Link href="/startNow">
-                            Try now
-                        </Link>
+                    <button className="w-32 mx-1 my-2 px-1 py-2 text-center cursor-pointer rounded-lg bg-green-500 hover:bg-green-400 text-gray-100 font-semibold">
+                        Get started
                     </button>
-                    <p> 40% discount on the first 3 months</p>
                 </div>
                 <div>
                     <HeaderImage/>
@@ -31,3 +28,6 @@ header.propTypes = {
 }
 
 export default header
+
+
+
--- src/components/headerImage.js
+++ src/components/headerImage.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const HeaderImage = props => {
     return (
         <div>
-            <img className="shadow rounded-lg shadow-2xl border" width="800" height="600" src="../images/vegetables.jpg" alt="my image" />
+            <img className="shadow rounded-lg shadow-2xl border" width="1000" height="600" src="../images/vegetables.jpg" alt="my image" />
         </div>
     )
 }
