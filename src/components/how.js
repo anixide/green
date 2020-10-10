b86d902630c82cbb784bfeb0e35ac3d7bdbeee08
g0thman <g.othman.anis@gmail.com>
2020-10-10 20:11:57 +0100

    Fixing pricing page

---
+++ src/components/how.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const how = () => {
+    return (
+        <section className="container mx-auto md:max-w-1/2 border border-black">
+            <div className="flex flex-col items-center space-y-6">
+                <h1 className="self-center text-5xl text-gray-700 font-bold text-center mt-10">How It Works</h1>
+                <p className="self-center max-w-3/4 text-center text-lg text-gray-700 font-light">Our goal is to make sure you always have everything you need to look, feel, and smell your best. Here’s how it all goes down:</p>
+                <button className="mx-4 my-2 px-6 py-2 text-center items-center cursor-pointer rounded-lg bg-green-500 hover:bg-green-400 text-gray-100 font-semibold"> Start Now</button>
+                <ul className="flex flex-wrap">
+                    <li className="w-1/2">
+                        <img src="../images/dsc.svg"></img>
+                        <h1 className="uppercase text-sm font-bold text-center">Get started</h1>
+                        <p className="text-sm text-center">Choose one of our risk-free starter sets and try what the Club has to offer.</p>
+                    </li>
+                    <li className="w-1/2">
+                        <img src="../images/dsc.svg"></img>
+                        <h1 className="uppercase text-sm font-bold text-center">Get started</h1>
+                        <p className="text-sm text-center">Choose one of our risk-free starter sets and try what the Club has to offer.</p>
+                    </li>
+                    <li className="w-1/2">
+                        <img src="../images/dsc.svg"></img>
+                        <h1 className="uppercase text-sm font-bold text-center">Get started</h1>
+                        <p className="text-sm text-center">Choose one of our risk-free starter sets and try what the Club has to offer.</p>
+                    </li>
+                    <li className="w-1/2">
+                        <img src="../images/dsc.svg"></img>
+                        <h1 className="uppercase text-sm font-bold text-center">Get started</h1>
+                        <p className="text-sm text-center">Choose one of our risk-free starter sets and try what the Club has to offer.</p>
+                    </li>
+                    <li className="w-1/2">
+                        <img src="../images/dsc.svg"></img>
+                        <h1 className="uppercase text-sm font-bold text-center">Get started</h1>
+                        <p className="text-sm text-center">Choose one of our risk-free starter sets and try what the Club has to offer.</p>
+                    </li>
+                    <li className="w-1/2">
+                        <img src="../images/dsc.svg"></img>
+                        <h1 className="uppercase text-sm font-bold text-center">Get started</h1>
+                        <p className="text-sm text-center">Choose one of our risk-free starter sets and try what the Club has to offer.</p>
+                    </li>
+                    
+                </ul>
+            </div>
+            
+        
+        </section>
+    )
+}
+
+export default how
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Header from "../components/header"
-import How from "./how"
+import How from "../components/how"
 import Pricing from "./pricing"
 
 export default function Home() {
   return (
-    <div className="relative max-h-auto">
+    <div className="">
+
         <Header/>
         <How />
+        <Pricing />
     </div>
   )
 }
